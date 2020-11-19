6a610d7b9c5c43813a4c729697bfbdbde7051de2
jesseleeruiz <jesseleeruiz@gmail.com>
2020-11-18 14:48:31 -1000

    Added Xcode project. Changed ContentView struct to AppetizerTabView.

---
+++ Food Deck/Food Deck/AppetizerTabView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Food Deck
+//
+//  Created by Jesse Ruiz on 11/18/20.
+//
+
+import SwiftUI
+
+struct AppetizerTabView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        AppetizerTabView()
+    }
+}
---
+++ Food Deck/Food Deck/Food_DeckApp.swift
@@ -0,0 +1,17 @@
+//
+//  Food_DeckApp.swift
+//  Food Deck
+//
+//  Created by Jesse Ruiz on 11/18/20.
+//
+
+import SwiftUI
+
+@main
+struct Food_DeckApp: App {
+    var body: some Scene {
+        WindowGroup {
+            AppetizerTabView()
+        }
+    }
+}
