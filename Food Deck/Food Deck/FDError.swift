376e152983372add68a2cc173a9bd379a7483254
jesseleeruiz <jesseleeruiz@gmail.com>
2020-11-29 15:59:02 -1000

    Created Network Manager.

---
+++ Food Deck/Food Deck/FDError.swift
@@ -0,0 +1,15 @@
+//
+//  FDError.swift
+//  Food Deck
+//
+//  Created by Jesse Ruiz on 11/29/20.
+//
+
+import Foundation
+
+enum FDError: Error {
+    case invalidURL
+    case invalidResponse
+    case invalidData
+    case unableToComplete
+}
