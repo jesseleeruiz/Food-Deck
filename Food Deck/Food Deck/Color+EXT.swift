a3867bdd7d441b755336e1e0398aa6c370049898
jesseleeruiz <jesseleeruiz@gmail.com>
2020-12-04 10:48:46 -1000

    Created a loading indicator view and added it to the main screen.

---
+++ Food Deck/Food Deck/Color+EXT.swift
@@ -0,0 +1,16 @@
+//
+//  Color+EXT.swift
+//  Food Deck
+//
+//  Created by Jesse Ruiz on 12/4/20.
+//
+
+import SwiftUI
+
+extension Color {
+    static let brandPrimary = Color("brandPrimary")
+}
+
+extension UIColor {
+    static let brandPrimary = UIColor(named: "brandPrimary")
+}
---
+++ Food Deck/Food Deck/LoadingView.swift
@@ -0,0 +1,31 @@
+//
+//  LoadingView.swift
+//  Food Deck
+//
+//  Created by Jesse Ruiz on 12/4/20.
+//
+
+import SwiftUI
+
+struct ActivityIndicator: UIViewRepresentable {
+    
+    func makeUIView(context: Context) -> UIActivityIndicatorView {
+        let activityIndicatorView = UIActivityIndicatorView(style: .large)
+        activityIndicatorView.color = UIColor.brandPrimary
+        activityIndicatorView.startAnimating()
+        return activityIndicatorView
+    }
+    
+    func updateUIView(_ uiView: UIActivityIndicatorView, context: Context) {}
+}
+
+struct LoadingView: View {
+    var body: some View {
+        ZStack {
+            Color(.systemBackground)
+                .edgesIgnoringSafeArea(.all)
+            
+            ActivityIndicator()
+        }
+    }
+}
